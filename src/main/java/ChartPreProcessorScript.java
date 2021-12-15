c779c63a389e54089504f4beba16705238fc85ac
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2021-12-15 10:10:21 -0800

    Script updates

---
+++ src/main/java/ChartPreProcessorScript.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Vector;
+
+public class ChartPreProcessorScript {
+    public static void main(String[] args){
+        String filePath = "C:\\Users\\Brenn\\Downloads\\Existing_Conditions-Trinity.dss";
+        Collection<String> locationsToChart = ChartPreProcessor.getAllSavedLocations(filePath);
+        ChartPreProcessor tool = new ChartPreProcessor(filePath,locationsToChart,500,"C:\\Temp\\ChartsForNRC\\");
+        tool.writeToExcel();
+    }
+}
+
