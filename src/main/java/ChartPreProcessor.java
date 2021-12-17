7666d69e3f37cb1a22ca7f4db0451189a093d040
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2021-12-16 19:25:22 -0800

    Chart Pre-processor updates

--- src/main/java/ChartPreProcessor.java
+++ src/main/java/ChartPreProcessor.java
@@ -26,7 +26,7 @@ public class ChartPreProcessor {
                 FileWriter myFileWriter = new FileWriter(_outputDir + locations + ".csv");
                 HecPairedData pairedDataA = new HecPairedData();
                 pairedDataA.setDSSFileName(_fileName);
-                Vector<DSSPathname> chartSerieses = getPathnamesForChart(locations);
+                Vector<DSSPathname> chartSerieses = getPathnamesForChart(locations,252);
                 for(DSSPathname series : chartSerieses){
                     PairedDataContainer pdcA = new PairedDataContainer();
                     pdcA.fullName = series.getPathname();
@@ -46,6 +46,7 @@ public class ChartPreProcessor {
                     }
                     myFileWriter.write("\n");
                 }
+                myFileWriter.write(locations + "\n");
                 myFileWriter.close();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -53,7 +54,7 @@ public class ChartPreProcessor {
         }
     }
 
-    public Vector<DSSPathname> getPathnamesForChart(String ePart) {
+    public Vector<DSSPathname> getPathnamesForChart(String ePart, int numRealsToChart) {
         Vector<String> chartSeriesList = new Vector<>();
         String[] recordsToCollect = new String[]{"Realization Thin","0.975 Confidence Limit","0.025 Confidence Limit","Frequency Thin"};
         HecDataManager dssManager = new HecDataManager();
@@ -61,7 +62,17 @@ public class ChartPreProcessor {
         for(String recordType : recordsToCollect){
             String pathWithWildChars = "/*/*/*/*" + recordType + "*/*"+ ePart + "*/*/";
             String[] pathnames = dssManager.getCatalog(false, pathWithWildChars);
-            chartSeriesList.addAll(Arrays.asList(pathnames));
+            if(recordType == "Realization Thin"){
+                for(int i = 0; i< pathnames.length; i++){
+                    chartSeriesList.add(pathnames[i]);
+                    if(i>=numRealsToChart-1){
+                        break;
+                    }
+                }
+            }
+            else {
+                chartSeriesList.addAll(Arrays.asList(pathnames));
+            }
         }
         Vector<DSSPathname> out = new Vector<>();
         for(String pathname : chartSeriesList){
--- src/main/java/ChartPreProcessorScript.java
+++ src/main/java/ChartPreProcessorScript.java
@@ -5,9 +5,9 @@ import java.util.Vector;
 
 public class ChartPreProcessorScript {
     public static void main(String[] args){
-        String filePath = "C:\\Users\\Brenn\\Downloads\\Existing_Conditions-Trinity.dss";
+        String filePath = "C:\\Users\\Brenn\\Downloads\\Existing_Conditions-Trinity (1).dss";
         Collection<String> locationsToChart = ChartPreProcessor.getAllSavedLocations(filePath);
-        ChartPreProcessor tool = new ChartPreProcessor(filePath,locationsToChart,500,"C:\\Temp\\ChartsForNRC\\");
+        ChartPreProcessor tool = new ChartPreProcessor(filePath,locationsToChart,500,"C:\\Temp\\FinalChartsForNRC\\");
         tool.writeToExcel();
     }
 }
